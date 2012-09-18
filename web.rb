b80343ef42d3a77a15237b82ad429eddabfb8a66
Kunal Bhat <kunalbhat@gmail.com>
2012-09-17 22:19:17 -0500

    Add haml template and sass style.

--- web.rb
+++ web.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
+
+get '/style.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+  scss :style
+end
 
 get '/' do
-  "Hello, world"
+  haml :index
 end
