f10d3e317b4f3d098cbb76c312fde12d0e8b142f
Kunal Bhat <kunalbhat@gmail.com>
2012-09-20 14:04:24 -0500

    Add public folder and images. Add content.

--- web.rb
+++ web.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 
+use Rack::Static, :urls => ["/images"], :root => "public"
+set :static, true
+set :root, File.dirname(__FILE__)
+
 get '/style.css' do
   scss :style
 end
