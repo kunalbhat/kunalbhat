973ef2f8a6e00c2262e1fa9df8d17e1a8bcda7f8
Kunal Bhat <kunalbhat@gmail.com>
2014-02-11 11:45:43 -0600

    Remove github_api bundles

--- vendor/ruby/2.1.0/gems/nokogiri-1.6.1/ext/nokogiri/tmp/x86_64-apple-darwin13.0.2/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/kunalbhat/Documents/kunalbhat/vendor/ruby/2.1.0/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/lib"
-XSLT_LIBS="-lxslt  -L/Users/kunalbhat/Documents/kunalbhat/vendor/ruby/2.1.0/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
-XSLT_INCLUDEDIR="-I/Users/kunalbhat/Documents/kunalbhat/vendor/ruby/2.1.0/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/include"
-MODULE_VERSION="xslt-1.1.26"
